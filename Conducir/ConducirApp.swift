73a8b4a75066e3415b95713b30e0f4852e75e014
manuelduarte077 <carlosd99m@gmail.com>
2025-04-04 00:07:56 -0600

    Initial Commit

---
+++ Conducir/ConducirApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConducirApp.swift
+//  Conducir
+//
+//  Created by Manuel Duarte on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ConducirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
